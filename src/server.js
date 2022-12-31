8f76029fca2b43f22cae97819424eb4c6234d1aa
HieuNguyen21 <nnguyenngochieu1@gmail.com>
2022-12-31 23:20:22 +0700

    hieu

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebRouter from './route/web'
+require('dotenv').config();
+
+let app = express();
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}))
+
+viewEngine(app);
+initWebRouter(app);
+
+let port = process.env.PORT || 6969;
+
+app.listen(port, () => {
+    console.log("Backeng Nodejs is runing on the port" + port)
+})
+
